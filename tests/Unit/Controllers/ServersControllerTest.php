4e832dfd81d0988998ee41a4dd884a545cdbbfe5
Brian O <boneill@splickit.com>
2018-12-23 15:42:05 -0500

    BT Controller

--- tests/Unit/Controllers/ServersControllerTest.php
+++ tests/Unit/Controllers/ServersControllerTest.php
@@ -27,6 +27,6 @@ class ServersControllerTest extends TestCase
     public function testGetLastGitPull() {
         $serversController = new ServersController();
         $lastGitPullTime = $serversController->getLastGitPullTime();
-        $this->assertEquals(1);
+        $this->assertEquals(1, $lastGitPullTime);
     }
 }
