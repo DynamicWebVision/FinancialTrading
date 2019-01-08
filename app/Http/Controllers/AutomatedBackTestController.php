3e792d1f7d5d395e3db10ba3e1ecc9b1283a0725
Brian O <boneill@splickit.com>
2019-01-08 15:07:24 -0500

    Test

--- app/Http/Controllers/AutomatedBackTestController.php
+++ app/Http/Controllers/AutomatedBackTestController.php
@@ -54,7 +54,7 @@ class AutomatedBackTestController extends Controller {
     }
 
     public function runAutoBackTestIfFailsUpdate() {
-        die();
+        xasdf
         Log::emergency('runAutoBackTestIfFailsUpdate starting');
 
         //Set Last Git Pull Time To Check Later
