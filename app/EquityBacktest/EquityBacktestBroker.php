5779603c0b7f8b188056369f66a67a1141a1429a
Brian O <boneill@splickit.com>
2019-02-06 15:11:25 -0600

    Change

--- app/EquityBacktest/EquityBacktestBroker.php
+++ app/EquityBacktest/EquityBacktestBroker.php
@@ -1,26 +1,64 @@
 <?php namespace App\EquityBacktest;
 use \Log;
-use App\Model\TdAmeritradeAccount;
+use App\Model\Stocks\StocksBackTestPositions;
+use App\Model\Stocks\Stocks;
+use App\Services\Utility;
 
 
 class EquityBackTestBroker {
     public $currentAccountTotalValue = 50000;
     public $openPositions = [];
     public $currentAccountAvailableCapital = 50000;
+    public $positionId = 0;
 
+    public $backTestIterationId;
 
-    public function newLongPosition() {
+    public $utility;
+
+    public function __construct() {
 
     }
 
-    public function newShortPosition() {
-        
+    public function newLongPosition($rate, $symbol, $shareCount, $stopLossRate) {
+        $this->newPosition($rate, $symbol, $shareCount, 1, $stopLossRate);
     }
 
-    public function setAccountCurrentInfo() {
-        return [
+    public function newShortPosition($rate, $symbol, $shareCount, $stopLossRate) {
+        $this->newPosition($rate, $symbol, $shareCount, -1, $stopLossRate);
+    }
 
-        ];
+    public function newPosition($rate, $symbol, $shareCount, $side, $stopLossRate) {
+        $this->positionId = $this->positionId + 1;
+
+        $stock = Stocks::where('symbol', '=', $symbol)->first();
+
+        $newBackTestPosition = new StocksBackTestPositions();
+
+        $newBackTestPosition->stocks_back_test_iteration_id = $this->backTestIterationId;
+
+        $newBackTestPosition->stock_id = $stock->id;
+        $newBackTestPosition->shares_count = $shareCount;
+        $newBackTestPosition->open_date = $rate->dateTime;
+        $newBackTestPosition->open_price = $rate->closeMid;
+
+        $newBackTestPosition->stop_loss_rate = $stopLossRate;
+
+        $newBackTestPosition->position_type = $side;
+
+        $newBackTestPosition->save();
+    }
+
+    public function closePosition($id) {
+
+    }
+
+    public function setAccountInfo() {
+        $openPositions = StocksBackTestPositions::where('stocks_back_test_iteration_id', '=', $this->backTestIterationId)
+                            ->where('close_price', '=', 0)
+                            ->get()
+                            ->toArray();
+
+        $this->openPositions = $openPositions;
     }
 
     public function endPeriod() {
--- app/EquityStrategy/EquityBaseStrategy.php
+++ app/EquityStrategy/EquityBaseStrategy.php
@@ -1,4 +1,4 @@
-<?php namespace App\ForexStrategy;
+<?php namespace App\EquityStrategy;
 
 use \Log;
 use \App\ForexBackTest\BackTestHelpers;
@@ -15,110 +15,7 @@ abstract class EquityBaseStrategy  {
         $this->broker = $broker;
     }
 
-    public function getAvailableMargin() {
-        $this->accountInfo = $this->oanda->accountInfo();
-        return $this->accountInfo->marginAvailable;
-    }
-
-    public function calculatePositionAmount() {
-        if (!$this->backtesting) {
-            $this->positionAmount = round(round($this->accountAvailableMargin)*$this->positionMultiplier);
-            $this->oanda->positionAmount = $this->positionAmount;
-
-            $this->strategyLogger->logMessage('Margin Available: '.$this->accountAvailableMargin.'Position Multiplier '.$this->positionMultiplier.' Position Amount: '.$this->positionAmount, 1);
-        }
-    }
-
-    public function checkOpenPositionsThreshold() {
-        if (!$this->backtesting) {
-            $count = $this->oanda->checkTotalOpenPositions();
-
-            if ($count <= $this->maxPositions) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-        else {
-            return true;
-        }
-    }
-
-    public function newLongPosition() {
-
-    }
-
-    public function newShortPosition() {
-
-    }
-
-    public function calculateLongStopLoss($buyPrice) {
-        return $buyPrice - ($this->exchange->pip * $this->stopLossPipAmount);
-    }
-
-    public function calculateShortStopLoss($price) {
-        return $price + ($this->exchange->pip * $this->stopLossPipAmount);
-    }
-
-    public function calculateLongTakeProfit($price) {
-        return $price + ($this->exchange->pip * $this->takeProfitPipAmount);
-    }
-
-    public function calculateShortTakeProfit($price) {
-        return $price - ($this->exchange->pip * $this->takeProfitPipAmount);
-    }
-
-    public function calculateLimitEndTime() {
-        $frequency = DecodeFrequency::where('oanda_code', '=', $this->oanda->frequency)->first();
-        return time() + ($frequency->frequency_seconds - 60);
-    }
-
-    public function modifyLongStopLoss($openPosition) {
-        if (!$this->backtesting) {
-            $this->oanda->stopLoss = $this->calculateLongStopLoss($this->currentPriceData->mid);
-
-            foreach ($openPosition['positionTradeIds'] as $tradeId) {
-                $this->oanda->modifyStopLoss($tradeId);
-            }
-        }
-        else {
-            $this->backTestPositions[sizeof($this->backTestPositions)-1]['stopLoss'] = $this->calculateLongStopLoss($this->currentPriceData->open);
-        }
-    }
-
-    public function modifyStopLoss($newPricePoint) {
-        if (!$this->backtesting) {
-            $this->oanda->stopLoss = $this->oanda->getOandaPrecisionPrice($newPricePoint, $this->exchange->pip);
-            foreach ($this->openPosition['positionTradeIds'] as $tradeId) {
-                $this->oanda->modifyStopLoss($tradeId);
-            }
-
-        }
-        else {
-            $this->backTestPositions[sizeof($this->backTestPositions)-1]['stopLoss'] = $newPricePoint;
-        }
-    }
-
-    public function modifyShortStopLoss($openPosition) {
-        if (!$this->backtesting) {
-            $this->oanda->stopLoss = $this->calculateShortStopLoss($this->currentPriceData->mid);
-            $this->oanda->modifyStopLoss($openPosition['stopLossId']);
-        }
-        else {
-            $this->backTestPositions[sizeof($this->backTestPositions)-1]['stopLoss'] = $this->calculateShortStopLoss($this->currentPriceData->open);
-        }
-    }
-
-    public function addTrailingStopToPosition($tsPipAmount) {
-
-    }
-
-    public function getCurrentAccountInfo() {
-
-    }
-
-    public function calculateAmountOfSharesToBuy($moneyToSpend, $sharePrice) {
-        return floor($moneyToSpend/$sharePrice);
+    public function newPeriodTasks() {
+        $this->broker->setAccountInfo();
     }
 }
