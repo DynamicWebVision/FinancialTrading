368c77ab5598164f470473b5b1fdf2d5c598ec7f
Brian O <boneill@splickit.com>
2019-11-02 23:00:35 -0700

    Aws Stuff

--- tests/Unit/Controllers/ServersControllerTest.php
+++ tests/Unit/Controllers/ServersControllerTest.php
@@ -122,6 +122,6 @@ class ServersControllerTest extends TestCase
 
     public function testSetServerId() {
         $serversController = new ServersController();
-        $serversController->backupDbWithImage();
+        $serversController->backupDbWithImageDeleteOld();
     }
 }
